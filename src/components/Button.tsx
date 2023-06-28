c5e4de9f1a13745a037f15d2bef62504b5383dc3
Mrleoneed <mrleoneed@gmail.com>
2023-06-28 15:53:05 +0300

    SuperButton & SuperInput

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type ButtonType = {
+    name: string
+    callBack: () => void
+}
+
+
+export const Button = (props: ButtonType) => {
+    const onClickButtonHandler = () => {
+        props.callBack()
+    }
+
+    return (
+
+        <button onClick={onClickButtonHandler}>{props.name}</button>
+
+    );
+};
